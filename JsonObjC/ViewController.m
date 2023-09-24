1ebff208450f589034748816c90941972c0548ba
azorinov <azorinov@hotmail.com>
2023-09-24 18:16:29 +0300

    json print

--- JsonObjC/ViewController.m
+++ JsonObjC/ViewController.m
@@ -15,8 +15,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSArray *days = [self JSONFromFile];
+    NSLog(@"Json data is here %@", days);
+    for (NSDictionary * item in days)
+    {
+        NSString *date = (NSString *)item[@"date"];
+        NSString *day = (NSString *)item[@"day"];
+        NSString *ID = (NSString *)item[@"id"];
+        NSLog(@"date: %@",date);
+        NSLog(@"day: %@",day);
+        NSLog(@"id: %@",ID);
+    }
 }
 
-
+- (id)JSONFromFile
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"sample" ofType:@"json"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    return [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+}
 @end
